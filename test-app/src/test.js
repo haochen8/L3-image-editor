a9e6b3e4472b7780bcce3a1706dfd6de113cdc39
hao.chen8 <hc222ig@student.lnu.se>
2024-10-22 15:44:55 +0200

    Add drag/drop and save

--- test-app/src/test.js
+++ test-app/src/test.js
@@ -6,7 +6,7 @@
  * @version 1.0.0
  */
 
-import { ImageProcessor, loadImage } from './index.js'
+import { ImageProcessor, loadImage } from '../../src/index.js'
 
 // Get elements from the DOM
 const fileInput = document.getElementById('fileInput')
@@ -18,38 +18,12 @@ const brightnessInput = document.getElementById('brightnessInput')
 const noiseButton = document.getElementById('noiseButton')
 const invertButton = document.getElementById('invertButton')
 const errorMessage = document.getElementById('errorMessage')
-const saveButton = document.getElementById('saveButton')
 
 // Get the canvas and context
 const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d', { willREADFrequently: true })
 
 let processor
-
-// Event listeners for the drop zone
-['dragenter', 'dragover', 'dragleave', 'drop'].forEach((eventName) => {
-  canvas.addEventListener(eventName, (event) => {
-    event.preventDefault()
-    event.stopPropagation()
-  })
-})
-
-canvas.addEventListener('drop', async (event) => {
-  const file = event.dataTransfer.files[0]
-  if (file) {
-    try {
-      const imageData = await loadImage(file)
-      // Save the original image data
-      processor = new ImageProcessor(imageData)
-      displayImage(processor.getImageData())
-    } catch (error) {
-      console.error(error)
-      errorMessage.textContent = 'Invalid image file, try png or jpg'
-    }
-  }
-})
-
-canvas.addEventListener('click', () => fileInput.click())
 
 // Event listener for the file input
 fileInput.addEventListener('change', async (event) => {
@@ -143,29 +117,6 @@ resetButton.addEventListener('click', () => {
   }
 })
 
-// Event listener for the save button
-saveButton.addEventListener('click', () => {
-  if (processor) {
-    errorMessage.textContent = ''
-    const link = document.createElement('a')
-    link.download = 'edited_image.png'
-    link.href = canvas.toDataURL('image/png')
-    link.click()
-  } else {
-    errorMessage.textContent = 'There is none image to save'
-  }
-})
-
-// Draw the canvas text
-function drawCanvasText() {
-  const context = canvas.getContext('2d')
-  context.clearRect(0, 0, canvas.width, canvas.height)
-  context.fillStyle = '#888';
-  context.font = '20px Arial';
-  context.textAlign = 'center';
-  context.fillText('Drop and release an image or click to upload', canvas.width / 2, canvas.height / 2);
-}
-
 /**
  * Display the image on the canvas.
  *
@@ -179,8 +130,3 @@ function displayImage (imageData) {
   // Draw the image data on the canvas
   context.putImageData(imageData, 0, 0)
 }
-
-// Rita instruktionstexten på canvas när sidan laddas
-canvas.width = 800;
-canvas.height = 600;
-drawCanvasText();
